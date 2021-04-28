9da4311469def459fe02d00572fe82d694f446ac
steven.allison <steven.allison@CRO-ICG04-SA>
2021-04-28 10:52:59 +0100

    Add project files.

---
+++ Trig/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180.0 / Math.PI);
+        }
+    }
+}
